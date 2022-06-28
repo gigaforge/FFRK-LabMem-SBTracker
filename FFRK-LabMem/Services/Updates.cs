227336ceadb91d1805761dde48d184f7078c3926
bover87 <108338035+bover87@users.noreply.github.com>
2022-06-28 14:46:54 +0200

    Update Updates.cs
    Minor updates to updating system.

--- FFRK-LabMem/Services/Updates.cs
+++ FFRK-LabMem/Services/Updates.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -23,8 +23,8 @@ namespace FFRK_LabMem.Services
         private HttpClient httpClient;
         private const String API_URL = "https://api.github.com/repos/{0}/{1}/releases";
         private const String WEB_URL = "https://github.com/{0}/{1}/releases";
-        private const string GITHUB_USER = "hughjeffner";
-        private const string GITHUB_REPO = "ffrk-labmem";
+        private const string GITHUB_USER = "bover87";
+        private const string GITHUB_REPO = "HMB-FFRK-JP-LabMem";
 
         public Updates(bool includePreRelease)
         {
